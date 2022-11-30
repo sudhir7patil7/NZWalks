953b0335388fe305c0dae9a3ced1445d76007302
SudhirPatil <sudhir7patil7@yahoo.com>
2022-11-30 11:30:57 +0530

    End of Video -19

---
+++ NZWalks.API/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
